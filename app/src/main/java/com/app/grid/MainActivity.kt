4a8bd795ab0ac3338d0767c4f5dffabbb0aebd7c
riyatyagi2021 <riya.mobcoder@gmail.com>
2021-08-12 12:49:17 +0530

    ButtonToast

--- app/src/main/java/com/app/grid/MainActivity.kt
+++ app/src/main/java/com/app/grid/MainActivity.kt
@@ -7,6 +7,7 @@ import android.view.Gravity
 import android.view.View
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.custom_toast.*
 
@@ -22,14 +23,11 @@ class MainActivity : AppCompatActivity() {
 
         button1.setOnClickListener(){
             //Toast.makeText(this,"Button is clicked",Toast.LENGTH_SHORT).show()
-
-
             var name=personname.editableText.toString()
 
             val intent=Intent(this,Dogs::class.java)
             intent.putExtra("name",name)
             startActivity(intent)
-
         }
 
 
@@ -43,6 +41,33 @@ class MainActivity : AppCompatActivity() {
           //  myToast.setGravity(Gravity.CENTER_VERTICAL, 0, 0)
             myToast.view = layout//setting the view of custom toast layout
             myToast.show()
+        }
+
+
+        button3.setOnClickListener(){
+                val builder = AlertDialog.Builder(this)
+
+            builder.setTitle(R.string.dialogTitle)
+            builder.setMessage(R.string.dialogMessage)
+            builder.setIcon(android.R.drawable.ic_dialog_alert)
+
+            builder.setPositiveButton("Yes"){dialogInterface, which ->
+                Toast.makeText(applicationContext,"clicked yes",Toast.LENGTH_SHORT).show()
+            }
+
+            builder.setNeutralButton("Cancel"){dialogInterface , which ->
+                Toast.makeText(applicationContext,"clicked cancel\n operation cancel",Toast.LENGTH_SHORT).show()
+            }
+
+            builder.setNegativeButton("No"){dialogInterface, which ->
+                Toast.makeText(applicationContext,"clicked No",Toast.LENGTH_SHORT).show()
+            }
+
+            val alertDialog: AlertDialog = builder.create()
+            alertDialog.setCancelable(false)
+            alertDialog.show()
+
+
         }
 
     }
